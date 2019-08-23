4277f7462248e1e6c4f31b3089912c1322e95df1
smrnjeet222 <smrnjeet222@gmail.com>
2019-08-23 22:50:00 +0530

    DS

---
+++ LinkedList.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+class node{
+    private:
+        int data;
+        node* next;
+    public:
+        node(){
+            data ;
+            next = NULL;
+        }
+        node(int d){
+            data = d;
+            next = NULL;
+        }
+        int len(node* head){
+            int len = 0;
+            while(head != NULL){
+                head = head->next;
+                len++;
+            }
+            return len;
+        }
+        void InsertAtHead(node*& head , int d){
+            node* n = new node(d);
+            n->next = head;
+            head = n;
+            return;
+        }
+        void InsertAtTail(node* &head , int d){
+            if(head == NULL){
+                head = new node(data);
+                return ;
+            }
+            node* tail = head;
+            while(tail->next != NULL){
+                tail = tail->next;
+            }
+            tail->next = new node(d);
+            return;
+
+        }
+        void InsertAtAnyPos(node*& head , int d , int pos){
+            if (head == NULL || pos == 0){
+                InsertAtHead(head , d);
+            }
+            else if(pos>len(head)){
+                InsertAtTail(head , d);
+            }
+            else{
+                int jump = 1;
+                node* temp = head;
+                while (jump < (pos-1)){
+                   temp = temp->next;
+                   jump++; 
+                }
+                node* n = new node(d);
+                n->next = temp->next;
+                temp->next = n; 
+                return;  
+            }
+        }
+        void print(node*head){
+    
+            while(head!=NULL){
+                cout<<head->data<<"->";
+                head = head->next;
+            }
+            cout<<endl;
+        }
+     
+};
+
+int main(){
+    node* head = NULL;
+    node N;
+
+    N.InsertAtHead(head , 5);
+    N.InsertAtHead(head , 4);
+    N.InsertAtHead(head , 3);
+    N.InsertAtTail(head , 10);
+    N.InsertAtTail(head , 12);
+    N.InsertAtAnyPos(head , 6 , 5);
+    cout<< endl<<N.len(head)<<endl;
+
+    N.print(head);
+
+
+    return 0;
+}
