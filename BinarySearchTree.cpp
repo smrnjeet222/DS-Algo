712ae63e52a361de5937c4983609fc8de498d77e
smrnjeet222 <smrnjeet222@gmail.com>
2019-09-03 23:17:45 +0530

    SearchBST

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -26,11 +26,21 @@ void Insert(node*& root, int d) {
     return;
 }
 
+node* Search(node* root, int key) {
+    if (root == NULL || root->data == key)
+        return root;
+    else if (key < root->data)
+        return Search(root->left, key);
+    else
+        return Search(root->right, key);
+}
+
 void print(node* root) {
     if (root != NULL) {
         print(root->left);
         cout << root->data << "-";
         print(root->right);
+        return;
     }
 }
 
@@ -38,13 +48,17 @@ int main() {
     node* root = NULL;
     Insert(root, 20);
     Insert(root, 10);
-    Insert(root , 50);
-    Insert(root , 5);
-    Insert(root , 12);
-    Insert(root , 25);
-    Insert(root , 33);
+    Insert(root, 50);
+    Insert(root, 5);
+    Insert(root, 12);
+    Insert(root, 25);
+    Insert(root, 33);
 
+    Search(root, 10) ? cout << "Key Found " : cout << "Key Missing ";
+
+    cout << endl;
     print(root);
+    cout << endl;
 
     return 0;
 }
