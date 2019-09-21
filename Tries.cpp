7104d8af815467b77a3ecaf6686ac19a720f51a4
smrnjeet222 <smrnjeet222@gmail.com>
2019-09-21 10:07:40 +0530

    Deletion

--- Tries.cpp
+++ Tries.cpp
@@ -38,6 +38,38 @@ bool search(TrieNode* root, string key) {
     }
     return (temp && temp->isEnd);
 }
+bool isEmpty(TrieNode* root){
+    for(int i=0 ; i<A_Size ; i++){
+        if(root->Children[i])
+            return false;
+    }
+    return true;
+}
+
+TrieNode* Delete(TrieNode* root , string key , int depth=0){
+    if(!root){
+        return root;
+    }
+
+    if(depth == key.size()){
+        if(root->isEnd){
+            root->isEnd = false;
+        }
+        if(isEmpty(root)){
+            delete(root);
+            root = NULL;
+        }
+        return root;
+    }
+
+    int index = key[depth] - 'a';
+    root->Children[index] = Delete(root->Children[index], key , depth+1);
+    if(isEmpty && root->isEnd==false){
+        delete(root);
+        root=NULL;
+    }
+    return root;
+}
 
 int main() {
     TrieNode* root = new TrieNode();
@@ -48,6 +80,7 @@ int main() {
                   "there",
                   "bye",
                   "hello",
+                  "her",
                   "his",
                   "hersey",
                   "their"};
@@ -57,13 +90,13 @@ int main() {
         insert(root , s[i]);
     }
 
-    search(root,"there") ? cout<<"Present\n" : cout<<"Not Present\n";
     search(root,"the") ? cout<<"Present\n" : cout<<"Not Present\n";
+    Delete(root , "the");
+    search(root,"there") ? cout<<"Present\n" : cout<<"Not Present\n";
     search(root,"them") ? cout<<"Present\n" : cout<<"Not Present\n";
     search(root,"here") ? cout<<"Present\n" : cout<<"Not Present\n";
+    Delete(root , "here");
     search(root,"her") ? cout<<"Present\n" : cout<<"Not Present\n";
-    search(root,"bye") ? cout<<"Present\n" : cout<<"Not Present\n";
-    search(root,"thick") ? cout<<"Present\n" : cout<<"Not Present\n";
 
     return 0;
 }
