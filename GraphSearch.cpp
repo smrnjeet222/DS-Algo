ae965c594c65bcd583b7ece4d3236360574541dc
Simranjeet Singh <smrnjeet222@gmail.com>
2019-10-04 15:55:19 +0530

    Depth First Search

--- GraphSearch.cpp
+++ GraphSearch.cpp
@@ -21,6 +21,19 @@ void BFS(int G[][7], int start, int n) {
     }
 }
 
+void DFS(int G[][7], int start, int n) {
+    static int visited[7] = {0};
+    int j;
+    if (visited[start] == 0) {
+        cout << start << " ";
+        visited[start] = 1;
+        for (j = 1; j < n; j++) {
+            if (G[start][j] == 1 && visited[j] == 0)
+                DFS(G, j, n);
+        }
+    }
+}
+
 int main() {
     int G[7][7] = { {0, 0, 0, 0, 0, 0, 0},
                     {0, 0, 1, 1, 0, 0, 0}, 
@@ -31,6 +44,8 @@ int main() {
                     {0, 0, 0, 0, 1, 0, 0} };
 
     BFS(G, 2 ,7);
+    cout<<endl;
+    DFS(G, 4, 7);
 
     return 0;
 }
