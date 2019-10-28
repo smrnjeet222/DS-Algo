bdb16ddb9c4dd17c6bfac480fc9979fb0f440f86
Simranjeet Singh <smrnjeet222@gmail.com>
2019-10-28 20:04:07 +0530

    UnWeighted Graph

---
+++ Graph2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <list>
+#include <map>
+#include <cstring>
+
+using namespace std;
+
+template <typename T>
+class Graph {
+private:
+    map<T, list<T>> adjList;
+
+public:
+    Graph() {
+    }
+    void AddEdge(T from, T to, bool bidir= true){
+        adjList[from].push_back(to);
+        if (bidir) {
+            adjList[to].push_back(from);
+        }
+    }
+    void print(){
+        for(auto row:adjList){
+            T key = row.first;
+            cout<<key<<"->";
+            for(T elem:row.second){
+                cout<<elem<<",";
+            }
+            cout << endl;
+        }
+    }
+
+};
+
+int main() {
+
+    Graph<string> g;
+
+    g.AddEdge("Amritsar", "Delhi");
+    g.AddEdge("Amritsar", "Jaipur");
+    g.AddEdge("Delhi", "Siachen");
+    g.AddEdge("Delhi", "Lucknow");
+    g.AddEdge("Delhi", "Agra");
+    g.AddEdge("Delhi", "Banglore");
+    g.AddEdge("Amritsar", "Siachen");
+
+    g.print();
+
+
+    return 0;
+}
