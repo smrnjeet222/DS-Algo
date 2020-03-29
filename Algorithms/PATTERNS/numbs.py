806a383d2672307f9c649e082e3c26a5e68bb886
smrnjeet222 <smrnjeet222@gmail.com>
2020-03-29 16:35:28 +0530

    -

--- Algorithms/PATTERNS/numbs.py
+++ Algorithms/PATTERNS/numbs.py
@@ -9,9 +9,9 @@ print("One")
 # 6 5 4 3 2 1
 # 7 6 5 4 3 2 1
 
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        print(i-j+1 , end=" ")
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(i-j+1, end=" ")
     print()
 
 print()
@@ -27,7 +27,7 @@ print("Two")
 
 for i in range(1, n+1):
     for j in range(1, i+1):
-        print(n-(i-j) , end=" ")
+        print(n-(i-j), end=" ")
     print()
 
 print()
@@ -41,12 +41,12 @@ print("Three")
 #  2 2 2 2 2 2
 # 1 1 1 1 1 1 1
 
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if j<(n-i+1):
-            print(" ",end="")
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if j < (n-i+1):
+            print(" ", end="")
         else:
-            print(n-i+1 , end=" ")
+            print(n-i+1, end=" ")
     print()
 
 print()
@@ -60,10 +60,10 @@ print("Four")
 #       1 2
 #        1
 
-for i in range(1,n+1):
-    print(" "*i,end="")
-    for j in range(n,i-1,-1):
-        print(n-j+1 , end=" ")
+for i in range(1, n+1):
+    print(" "*i, end="")
+    for j in range(n, i-1, -1):
+        print(n-j+1, end=" ")
     print()
 
 print()
@@ -79,8 +79,8 @@ print("Five")
 
 for i in range(1, n+1):
     for j in range(1, n+1):
-        if i==j:
-            print(j , end=" ")
+        if i == j:
+            print(j, end=" ")
         else:
             print(0, end=" ")
     print()
@@ -96,17 +96,17 @@ print("Six")
 # 1 6 6 6 6 6 6
 # 7 7 7 7 7 7 7
 
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if j<=(n-i):
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if j <= (n-i):
             print(1, end=" ")
         else:
-            print(i , end=" ")
+            print(i, end=" ")
     print()
 
 print()
 # ----------------------------------------------
-print("Seven")    
+print("Seven")
 # 4 3 2 1
 # 3 2 1
 # 2 1
@@ -116,10 +116,10 @@ print("Seven")
 # 3 2 1
 # 4 3 2 1
 
-for i in range(-n , n+1):
-    if i!=0:
+for i in range(-n, n+1):
+    if i != 0:
         for j in range(1, abs(i)+1):
-            print(abs(i)-j+1 , end=" ")
+            print(abs(i)-j+1, end=" ")
         print()
 
 print()
@@ -136,27 +136,26 @@ print("Eight")
 #  2 3 4 5
 # 1 2 3 4 5
 
-for i in range(-n,n+1):
-    if i!=0:
+for i in range(-n, n+1):
+    if i != 0:
         print(" "*(n-abs(i)), end="")
-        for j in range(1,abs(i)+1):
-            print(n-(abs(i)-j+1)+1 , end=" ")
+        for j in range(1, abs(i)+1):
+            print(n-(abs(i)-j+1)+1, end=" ")
         print()
 
 print()
 # ----------------------------------------------
 print("Nine")
-# 1
-# 1 2 1
-# 1 2 3 2 1
-# 1 2 3 4 3 2 1
-# 1 2 3 4 5 4 3 2 1
+#          1
+#        1 2 1
+#      1 2 3 2 1
+#    1 2 3 4 3 2 1
+#  1 2 3 4 5 4 3 2 1
 
-for i in range(1,n+1):
-    for j in range(1, i):
-        print(j , end=" ")
-    for j in range(i , 0 , -1):
-        print(j , end=" ")
+for i in range(1, n+1):
+    print("  "*(n-i) , end=" ")
+    for j in range(-i+1, i):
+        print(i-abs(j), end=" ")
 
     print()
 
@@ -169,13 +168,97 @@ print("Ten")
 # 4 5 4
 # 5
 
-for i in range (1,n+1):
+for i in range(1, n+1):
     for j in range(-n+i, n-i+1):
-        print(n-abs(j) , end=" ")
+        print(n-abs(j), end=" ")
 
     print()
 
 print()
 # ----------------------------------------------
+print("Eleven")
+# 1
+# 2 2
+# 3 3 3
+# 4 4 4 4
+# 3 3 3
+# 2 2
+# 1
+
+for i in range(-n+1, n):
+    for j in range(1, n-abs(i)+1):
+        print(n-abs(i), end=" ")
+
+    print()
+
+print()
+# ----------------------------------------------
+print("Twelve")
+# 4
+# 3 4 3
+# 2 3 4 3 2
+# 1 2 3 4 3 2 1
+# 2 3 4 3 2
+# 3 4 3
+# 4
+
+for i in range(-n+1, n):
+    for j in range(-(n-abs(i))+1, n-abs(i)):
+        print(n-abs(j), end=" ")
+
+    print()
+
+print()
+# ----------------------------------------------
+print("Thirteen")
+#      1
+#     1 2
+#    1 2 3
+#   1 2 3 4
+#  1 2 3 4 5
+#   1 2 3 4
+#    1 2 3
+#     1 2
+#      1
+
+for i in range(-n+1 , n):
+    print(" "*abs(i) , end=" ")
+    for j in range(1, n-abs(i)+1):
+        print(j , end=" ")
+    
+    print()
+print()
+# ----------------------------------------------
+print("Fourteen")
+# 1             1
+# 1 2         2 1
+# 1 2 3     3 2 1
+# 1 2 3 4 4 3 2 1
+
+for i in range(1,n+1):
+    for j in range(-n , n+1):
+        if (n-abs(j)+1)<=i:
+            print(n-abs(j)+1, end=" ")
+        elif j!=0:
+            print(" ", end=" ")
+    print()
+
+print()
+# ----------------------------------------------
+print("Fifthteen")
+# 1 2 3 4 5 4 3 2 1
+# 1 2 3 4   4 3 2 1
+# 1 2 3       3 2 1
+# 1 2           2 1
+# 1               1
+
+for i in range(n,0,-1):
+    for j in range(-n+1, n):
+        if n-abs(j) <= i:
+            print(n-abs(j), end=" ")
+        else :
+            print(" ",end=" ")
+
+    print()
 
 
