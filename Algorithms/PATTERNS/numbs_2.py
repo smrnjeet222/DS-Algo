9d309b3b45b5a515bd1834495194486cc95ff3c9
smrnjeet222 <smrnjeet222@gmail.com>
2020-03-31 18:29:40 +0530

    number pattern

--- Algorithms/PATTERNS/numbs_2.py
+++ Algorithms/PATTERNS/numbs_2.py
@@ -162,12 +162,90 @@ print("Eight")
 #      1 0 1
 #        0
 
-for i in range(-n,n):
+for i in range(-n, n):
     print("  "*abs(i), end=' ')
     for j in range(-n+abs(i)+1, n-abs(i)):
-        print(abs(j) , end=" ")
+        print(abs(j), end=" ")
     print()
 
 print()
 # ----------------------------------------------
+print("Nine")
+# 1 2 3 4 5 4 3 2 1
+# 2 2 3 4 5 4 3 2 2
+# 3 3 3 4 5 4 3 3 3
+# 4 4 4 4 5 4 4 4 4
+# 5 5 5 5 5 5 5 5 5
+# 4 4 4 4 5 4 4 4 4
+# 3 3 3 4 5 4 3 3 3
+# 2 2 3 4 5 4 3 2 2
+# 1 2 3 4 5 4 3 2 1
 
+for i in range(-n+1, n):
+    for j in range(-n+1, n):
+        if abs(i) <= abs(j):
+            print(n-abs(i), end=' ')
+        else:
+            print(n-abs(j), end=' ')
+    print()
+
+print()
+# ----------------------------------------------
+print("Ten")
+# 1 2 3 4 5
+# 5 1 2 3 4
+# 4 5 1 2 3
+# 3 4 5 1 2
+# 2 3 4 5 1
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print((j-i+1) if j>=i else (n-i+j+1), end=' ')
+    print()
+
+print()
+# ----------------------------------------------
+print("Eleven")
+# 18                      9
+# 18 14                7  9
+# 18 14 10          5  7  9
+# 18 14 10 6     3  5  7  9
+# 18 14 10 6  2  3  5  7  9
+# 18 14 10 6     3  5  7  9
+# 18 14 10          5  7  9
+# 18 14                7  9
+# 18                      9
+
+for i in range(-n+1, n):
+    for j in range(-n+1, n):
+        t = abs(j)*4 + 2
+        if abs(j) < abs(i):
+            print("  ", end=' ')
+        else:
+            if j<=0:
+                print("%-3d"% t, end='')
+            else:
+                print("%-3d" % (t//2), end='')
+    print()
+
+print()
+# ----------------------------------------------
+print("Twelve")
+#             1
+#          2 1 1 2
+#       3 2 1   1 2 3
+#    4 3 2 1     1 2 3 4
+# 5 4 3 2 1       1 2 3 4 5
+#    4 3 2 1     1 2 3 4
+#       3 2 1   1 2 3
+#          2 1 1 2
+#             1
+
+for i in range(-n+1, n):
+    print("   "*abs(i) ,end='')
+    for j in range(n-abs(i), 0, -1):
+        print(j, end=" ")
+    print("  "*(n-abs(i)-2), end="")
+    for j in range(2 if abs(i)+1==n else 1, n-abs(i) +1):
+        print(j ,end=' ')
+    print()
