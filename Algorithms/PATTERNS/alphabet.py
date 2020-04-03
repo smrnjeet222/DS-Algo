e3d48a8afb4a3e13662a5888dde702d8653141a0
smrnjeet222 <smrnjeet222@gmail.com>
2020-04-04 00:08:57 +0530

    Alphabets Pattern

--- Algorithms/PATTERNS/alphabet.py
+++ Algorithms/PATTERNS/alphabet.py
@@ -92,3 +92,77 @@ print("Six")
 for i in range(0, n):
     print("0 "*i + chr(i+65) + " 1"*(n-i-1))
 
+print()
+# ___________________________________________
+print("Seven")
+# Aa
+# AaBb
+# AaBbCc
+# AaBbCcDd
+# AaBbCcDdEe
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(chr(j+64)+chr(j+96), end='')
+    print()
+
+print()
+# ___________________________________________
+print("Eight")
+#     A
+#    B C
+#   D E F
+#  G H I J
+# K L M N O
+
+c=0
+for i in range(1,n+1):
+    print(" "*(n-i), end="")
+    for j in range(1,i+1):
+        print(chr((c%26)+65), end=" ")
+        c+=1
+    print()
+
+print()
+# ___________________________________________
+print("Nine")
+#     A
+#    a a
+#   B B B
+#  b b b b
+# C C C C C
+#  c c c c
+#   D D D
+#    d d
+#     E
+
+c=1
+for i in range(-n+1 ,n):
+    print(" "*(abs(i)), end='')
+    if c%2:
+        print((chr(c//2+65) + " ")*(n-abs(i)))
+    else:
+        print((chr(c//2+96)+ " ")*(n-abs(i)))
+    c+=1
+
+print()
+# ___________________________________________
+print("Ten")
+# A B C
+#  Z Y
+# A B
+#  Z
+# A
+#  Z
+# A B
+#  Z Y
+# A B C
+
+for i in range(-n+1,n):
+    x=abs(i)+1
+    for j in range((2-x%2), x+1, 2):
+        if x%2:
+            print(chr(j//2+65), end=' ')
+        else:
+            print(" " + chr(91-j//2), end='')
+    print()
