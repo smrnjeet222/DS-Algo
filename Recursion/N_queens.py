8d77e4f41becdffa8735210a3ac912d74b7ce7fa
Simranjeet Singh <smrnjeet222@gmail.com>
2020-05-13 16:20:41 +0530

    Create N_queens.py

---
+++ Recursion/N_queens.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+N = 4
+
+def isSafe(board, row, col):
+    for i in range(col): 
+        if board[row][i] == 1: 
+            return False
+  
+    for i, j in zip(range(row, -1, -1),  
+                    range(col, -1, -1)): 
+        if board[i][j] == 1: 
+            return False
+
+    for i, j in zip(range(row, N, 1),  
+                    range(col, -1, -1)): 
+        if board[i][j] == 1: 
+            return False
+
+    return True
+
+
+def solveNQUtil(board, col):
+    if col >= N:
+        return True
+
+    for i in range(N):
+        if isSafe(board, i, col):
+            board[i][col] = 1
+
+            if solveNQUtil(board, col + 1) == True: 
+                return True
+
+            board[i][col] = 0
+
+    return False
+
+def solveNQ(): 
+    board = [ [0, 0, 0, 0], 
+              [0, 0, 0, 0], 
+              [0, 0, 0, 0], 
+              [0, 0, 0, 0] ] 
+
+    if solveNQUtil(board, 0) == False: 
+        print ("Solution does not exist") 
+        return False
+
+    print(np.matrix(board))
+    return True
+
+solveNQ() 
